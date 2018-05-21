c996b4575fb74541a3c91cce11b0742d565a88d1
Loi Chii Lek <loichiilek@gmail.com>
2018-05-21 10:37:14 +0800

    minor changes to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "This is my page :)"
+    if page_title.empty?
+      return base_title
+    else
+      return "This is my #{page_title} page :)"
+    end
+  end
 end
